b2d2e86dc13153b0ea1c6bd831b3a64d607695c7
Patrick <patrick.brophy2@gmail.com>
2023-08-24 23:50:47 -0500

    added custom filler value support

--- main.py
+++ main.py
@@ -4,34 +4,34 @@ import csv
 
 HEADER = ["_xddid", "doi", "_gddid", "title", "type", "journal", "link_type", "link", "author", "publisher", "volume", "pages", "year"]
 
-def bibjson_entry_to_row(entry):
+def bibjson_entry_to_row(entry, filler):
     row = []
     
-    xddid = ""
-    doi = ""
+    xddid = filler 
+    doi = filler
     for ids in entry.get("identifier", []):
             if ids["type"] == "doi":
                 doi = ids["id"]
             if ids["type"] == "_xddid":
                 xddid = ids["id"]
 
-    gddid = entry.get("_gddid", "")
-    title = entry.get("title", "")
-    entry_type = entry.get("type", "")
-    journal = entry.get("journal", {}).get("name", "")
-    link = entry.get("link", [{}])[0].get("url", "")
-    link_type = entry.get("link", [{}])[0].get("type", "")
+    gddid = entry.get("_gddid", filler)
+    title = entry.get("title", filler)
+    entry_type = entry.get("type", filler)
+    journal = entry.get("journal", {}).get("name", filler)
+    link = entry.get("link", [{}])[0].get("url", filler)
+    link_type = entry.get("link", [{}])[0].get("type", filler)
     author = '; '.join([item['name'] for item in entry.get('author', [])])
-    publisher = entry.get("publisher", "")
-    volume = entry.get("volume", "")
-    pages = entry.get("pages", "")
-    year = entry.get("year", "")    
+    publisher = entry.get("publisher", filler)
+    volume = entry.get("volume", filler)
+    pages = entry.get("pages", filler)
+    year = entry.get("year", filler)    
 
     row = [xddid, doi, gddid, title, entry_type, journal, link_type, link, author, publisher, volume, pages, year]
 
     return row
 
-def bibJSONtoCSV(in_file, out_file):
+def bibJSONtoCSV(in_file, out_file, filler):
     bibjson_data = json.load(in_file) 
 
     with out_file as f:
@@ -39,7 +39,7 @@ def bibJSONtoCSV(in_file, out_file):
         writer.writerow(HEADER)
         
         for entry in bibjson_data:
-            row = bibjson_entry_to_row(entry)
+            row = bibjson_entry_to_row(entry, filler)
             writer.writerow(row)
 
 if __name__ == "__main__":
@@ -47,7 +47,10 @@ if __name__ == "__main__":
 
     parser.add_argument("-i", "--infile", help="BibJSON file path", type=argparse.FileType('r'), required=True)
     parser.add_argument("-o", "--outfile", help="output file path", type=argparse.FileType('w'), required=True)
+    parser.add_argument("-f", "--missing-column-filler", 
+                        help="value that should be used as the filler for missing values in a column. the default is just an empty string", 
+                        type=str, default="", required=False)
 
     args = parser.parse_args()
 
-    bibJSONtoCSV(args.infile, args.outfile)
+    bibJSONtoCSV(args.infile, args.outfile, args.missing_column_filler)
