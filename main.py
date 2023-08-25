f8533477b3ac5846f5b735284a352a9b9dbd7f86
Patrick <patrick.brophy2@gmail.com>
2023-08-24 23:28:51 -0500

    basic parser logic done, crashes if missing entry. fix in next commit

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 import argparse
 import json
 import csv
+import pprint
 
 HEADER = ["_xddid", "doi", "_gddid", "title", "type", "journal", "link_type", "link", "author", "publisher", "volume", "pages", "year"]
 
 def bibjson_entry_to_row(entry):
     row = []
+    
+    xddid = ""
+    doi = ""
+    try:
+        for ids in entry["identifier"]:
+            if ids["type"] == "doi":
+                doi = ids["id"]
+            if ids["type"] == "_xddid":
+                xddid = ids["id"]
+        
+        gddid = entry["_gddid"]
+        title = entry["title"]
+        entry_type = entry["type"]
+        journal = entry["journal"]["name"]
+        link = entry["link"][0]["url"]
+        link_type = entry["link"][0]["type"]
+        author = '; '.join([item['name'] for item in entry['author']])
+        publisher = entry["publisher"]
+        volume = entry["volume"]
+        pages = entry["pages"]
+        year = entry["year"]
+    except Exception as e:
+        print(e)
+        pprint.pprint(entry)
+        exit(0)
+
+
+    row = [xddid, doi, gddid, title, entry_type, journal, link_type, link, author, publisher, volume, pages, year]
+
     return row
 
 def bibJSONtoCSV(in_file, out_file):
